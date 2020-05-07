f65c03b029d2ed2b8d3635cf1c1cb03c0aaa8da3
alex-whan <whan.alexander@gmail.com>
2020-05-06 19:04:13 -0700

    Added global hourly totals/footer rendering logic

--- js/app.js
+++ js/app.js
@@ -31,6 +31,7 @@ Store.prototype.cookieSalesSimulator = function () {
   // for loop through the array of random customers
     // multiply each customer entry by the average cookie sale
     // push into the cookiesSoldEachHour array
+    // push into the globalCookiesTotalEachHour array
   for (var i=0; i<this.customersEachHour.length; i++){
     var totalCookies = Math.ceil(this.customersEachHour[i] * this.avgCookiesPerCustomer);
     this.cookiesSoldEachHour.push(totalCookies);
@@ -96,20 +97,16 @@ Store.prototype.getRandomNumber = function (min, max) {
 };
 
 
-// Header rendering function - separate (not a prototype)
+// Header rendering function
 function renderHeader() {
   var parentElement = document.getElementById('table');
-
   // This is for the "Seattle" store (or "name")
   // Create a table row <tr>
   var tableRow = document.createElement('tr');
-
     // Create a <th>/<td> to stick onto table row <tr>
   var tableHeader = document.createElement('th')
-
     // Fill it with content: this.name
-  tableHeader.textContent = hours;
-
+  tableHeader.textContent = '';
     // Append <th> to the table row/<tr> we created (in this case, ROW is the parent, and <th> gets appended to its OWN parent (<tr>))
     // Want to append all <td>/<th> to the table row, then append ROW to TABLE once done
   tableRow.appendChild(tableHeader);
@@ -118,7 +115,7 @@ function renderHeader() {
     // Loop over the cookies sold each hour
   for(var i=0; i<hours.length; i++){
     // create a <td>
-    var tableData = document.createElement('td');
+    var tableData = document.createElement('th');
     // fill it with content: this.cookiesSoldEachHour[i]
     tableData.textContent = hours[i];
     // append <td> to the table row
@@ -127,14 +124,41 @@ function renderHeader() {
     // Append whole table row <tr> to the parent (<tbody>)
   parentElement.appendChild(tableRow);
   }
-  tableHeader.textContent = '';
+  
+};
+
+
+// Footer rendering function
+// Nested for loop to loop through hourly total at each location
+// for every hour -> for every store
+function renderFooter() {
+  // Select parent element (table)
+  // Create a table row <tr>
+  var parentElement = document.getElementById('table');
+  var tableRow = document.createElement('tr');
+  var tableHeader = document.createElement('th');
+  tableHeader.textContent = 'Totals';
+  tableRow.appendChild(tableHeader);
+
+  for(var i=0; i<hours.length; i++){
+    // Global variable that resets to zero each iteration
+    var globalHourlyTotal = 0;
+    // Inner for loop to add hourly sales at each store to global hourly sales
+    for(var j=0; j<allStores.length; j++){
+      globalHourlyTotal += allStores[j].cookiesSoldEachHour[i];
+    }
+
+        // Create a <th>/<td> to stick onto table row <tr>
+      var tableHeader = document.createElement('th')
+        // Fill it with content: this.name
+      tableHeader.textContent = globalHourlyTotal;
+        // Append <th> to the table row/<tr> we created (in this case, ROW is the parent, and <th> gets appended to its OWN parent (<tr>))
+        // Want to append all <td>/<th> to the table row, then append ROW to TABLE once done
+      tableRow.appendChild(tableHeader);
+  }
+  parentElement.appendChild(tableRow);     
+  
 }
-
-// Footer rendering function - separate (not a protoype) - use nested FOR loops
-
-
-
-
 
 var seattle = new Store('Seattle', 23, 65, 6.3);
 var tokyo = new Store('Tokyo', 3, 24, 1.2);
@@ -142,10 +166,13 @@ var dubai = new Store('Dubai', 11, 38, 3.7);
 var paris = new Store('Paris', 20, 38, 2.3);
 var lima = new Store('Lima', 2, 16, 4.6);
 
+var allStores = [seattle, tokyo, dubai, paris, lima];
+
 renderHeader();  
 seattle.render();
 tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
+renderFooter();
 
